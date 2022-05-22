c18b0e7d61b31dd852950438102df0aa16650e11
oinsio <oinsio.srv@gmail.com>
2022-05-22 11:39:05 +0600

    Add Spock Framework support for unit and integration tests

---
+++ src/test/groovy/guru/springframework/spring5webapp/Spring5webappApplicationTests.groovy
@@ -0,0 +1,14 @@
+package guru.springframework.spring5webapp
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class Spring5webappApplicationTests extends Specification {
+
+    def "should work"() {
+        expect:
+            true
+    }
+
+}
